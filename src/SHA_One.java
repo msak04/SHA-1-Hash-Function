37af44dcaa45cb2b83db7b35f7ab9129cd37b894
Saif Ahammod Khan <saif.ahammod@northsouth.edu>
2021-01-19 23:56:38 +0600

    File search found

--- src/SHA_One.java
+++ src/SHA_One.java
@@ -1,9 +1,31 @@
+import java.io.File;
+import java.io.FilenameFilter;
 
 public class SHA_One {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String pathlocator = FileLocator();
+		
+	}
+	
+	private static String FileLocator() {
+		// TODO Auto-generated method stub
+		
+		File dir = new File(".");
+		FilenameFilter filter = new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.startsWith("java SHA_One");
+			}
+		};
+		String[] children = dir.list(filter);
+		if (children == null) {
+			System.out.println("No file found.");
+			return null;
+		} else {
+			String filename = children[0];
+			return filename;
+		}
 	}
 
 }
